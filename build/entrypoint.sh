388f61dc831a74917abf409bb2d6040cc5a271fb
Cryptomental <cryptomental.com@gmail.com>
2018-10-17 10:24:17 +0200

    DNP_CORE: Follow the Filesystem Hierarchy Standard of Linux.
    The current data path is installed under /usr/src, a directory reserved
    for linux kernel sources. Follow the Filesystem Hierarchy Standard
    of Linux, and use /opt/dappnode instead.
    
    Refs: https://github.com/dappnode/DAppNode/issues/39

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-docker-compose -f /usr/src/app/DNCORE/docker-compose-bind.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-ipfs.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-ethchain.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-ethforward.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-vpn.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-wamp.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-dappmanager.yml up -d
-docker-compose -f /usr/src/app/DNCORE/docker-compose-admin.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-bind.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-ipfs.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-ethchain.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-ethforward.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-vpn.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-wamp.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-dappmanager.yml up -d
+docker-compose -f /opt/app/DNCORE/docker-compose-admin.yml up -d
 
-if [ -n "`grep \"restart: always\" /usr/src/app/DNCORE/docker-compose-core.yml`" ]; then
-    sed -i 's/restart: always//g' /usr/src/app/DNCORE/docker-compose-core.yml 
-    docker-compose -f /usr/src/app/DNCORE/docker-compose-core.yml up -d
+if [ -n "`grep \"restart: always\" /opt/app/DNCORE/docker-compose-core.yml`" ]; then
+    sed -i 's/restart: always//g' /opt/app/DNCORE/docker-compose-core.yml 
+    docker-compose -f /opt/app/DNCORE/docker-compose-core.yml up -d
 fi
