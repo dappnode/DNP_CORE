7b2599e5ef2c1c2ebf29b00f04dd8399a4d83037
vdo <vdo@greyfaze.net>
2019-12-18 14:53:57 +0100

    Prevent restart on manifest

--- build/upgrades/upgrade_003.sh
+++ build/upgrades/upgrade_003.sh
@@ -4,4 +4,4 @@ cp ./scripts/install_usbmount.sh /usr/src/app/DNCORE/scripts/upgrade
 chmod +x /usr/src/app/DNCORE/scripts/upgrade/install_usbmount.sh
 
 # Run script on the host with the nsenter tool
-docker run --privileged --pid=host -t alpine:3.8 nsenter -t 1 -m -u -n -i /usr/src/dappnode/DNCORE/scripts/upgrade/install_usbmount.sh
+docker run --rm --privileged --pid=host -t alpine:3.8 nsenter -t 1 -m -u -n -i /usr/src/dappnode/DNCORE/scripts/upgrade/install_usbmount.sh
