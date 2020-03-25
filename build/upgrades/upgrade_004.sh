a1b31f633bad5a23155b55ca6876a0ab66c891f8
vdo <vdo@greyfaze.net>
2020-03-25 17:21:46 +0100

    Glob for commands

--- build/upgrades/upgrade_004.sh
+++ build/upgrades/upgrade_004.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Upgrade from 0.2.26 to 0.2.27
+# Upgrade from 0.2.25 to 0.2.26
 # Fix command so it's non blocking profile at login
 grep -qF 'docker run --rm -v dncore_vpndnpdappnodeeth_data:/usr/src/app/secrets' /usr/src/app/DNCORE/.dappnode_profile ||
     sed -i "s,docker run --rm -v dncore_vpndnpdappnodeeth_data:/usr/src/app/secrets \$\(docker inspect DAppNodeCore-vpn.dnp.dappnode.eth --format '{{.Config.Image}}'\) getAdminCredentials,docker run --rm -ti -v dncore_vpndnpdappnodeeth_data:/usr/src/app/secrets \$\(docker inspect DAppNodeCore-vpn.dnp.dappnode.eth --format '{{.Config.Image}}'\) getAdminCredentials," /usr/src/app/DNCORE/.dappnode_profile
