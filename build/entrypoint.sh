303db3a029c81990c6c28bfbcc5542cf7028f170
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-04-20 20:10:54 +0200

    Add runOrder

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+STATUS_CHECK_DELAY=1m
+CONTAINER_NAME=DAppNodeCore-dappmanager.dnp.dappnode.eth
+DAPPMANAGER_YML=/usr/src/dappnode/DNCORE/docker-compose-dappmanager.yml 
 
 # Copy host scripts and packages
 mkdir -p /usr/src/app/DNCORE/scripts/upgrade
@@ -12,4 +15,20 @@ for filename in ./upgrades/upgrade_*.sh; do
     sh "${filename}"
 done
 
-sleep 1m
+sleep $STATUS_CHECK_DELAY
+
+STATUS=$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)
+RUNNING_IMAGE=$(docker inspect -f '{{.Config.Image}}' $CONTAINER_NAME)
+YML_IMAGE=$(cat $DAPPMANAGER_YML | awk '/image/{print $2}' | tr -d "'")
+
+if [[ $STATUS != "true" ]]; then
+    docker-compose -f $DAPPMANAGER_YML up -d <&-
+else
+    RUNNING_VERSION=$(echo $RUNNING_IMAGE | awk -F":" '{print $2}')
+    YML_VERSION=$(echo $YML_IMAGE | awk -F":" '{print $2}')
+    if [ "$(printf '%s\n' "$RUNNING_VERSION" "$YML_VERSION" | sort -V | head -n1)" != "$YML_VERSION" ]; then
+            docker-compose -f $DAPPMANAGER_YML up -d <&-
+    fi
+fi
+
+
