2c32dad859a815211c01f612f9aa3cdfbda60fac
pablo <mendez4a@gmail.com>
2021-06-09 18:00:45 +0200

    Add new aliases to profile and new script

---
+++ build/hostScripts/dappnode_access_credentials.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# This script will iterate over the access methods in dappnode
+# and display its credentials based on priority.
+# PRIORITY: Wi-Wi > Avahi > VPN (Wireguard > OpenVpn)
+
+#############
+#0.VARIABLES#
+#############
+# Containers
+WIFI_CONTAINER="DAppNodeCore-wifi.dnp.dappnode.eth"
+WIREGUARD_CONTAINER="DAppNodeCore-api.wireguard.dnp.dappnode.eth"
+OPENVPN_CONTAINER="DAppNodeCore-vpn.dnp.dappnode.eth"
+HTTPS_CONTAINER="DAppNodeCore-https.dnp.dappnode.eth"
+# Credentials
+WIREGUARD_GET_CREDS="docker exec -i $WIREGUARD_CONTAINER getWireguardCredentials"
+OPENVPN_GET_CREDS="docker exec -i $OPENVPN_CONTAINER getAdminCredentials"
+WIFI_GET_CREDS=$(cat /usr/src/dappnode/DNCORE/docker-compose-wifi.yml 2> /dev/null | grep 'SSID\|WPA_PASSPHRASE')
+# Endpoints
+AVAHI_ENDPOINT="dappnode.local"
+DAPPNODE_ADMINUI_URL="http://my.dappnode"
+DAPPNODE_ADMINUI_LOCAL_URL="http://${AVAHI_ENDPOINT}"
+DAPPNODE_WELCOME_URL="http://welcome.dappnode"
+
+#############
+#1.FUNCTIONS#
+#############
+
+function dappnode_startup_delay () {
+  echo "Wait until DAppNode initializes..."
+  sleep 10
+}
+
+# $1 Connection method $2 Credentials
+function create_connection_message () {
+  echo -e "\n\e[32mConnect to DAppNode through $1 using the following credentials:\e[0m\n$2\n\nVisit \e[4m$DAPPNODE_ADMINUI_URL\e\n\n[0mCheck out all the access methods available to connect to your DAppNode at \e[4m$DAPPNODE_WELCOME_URL\e[0m\n"
+}
+
+function wifi_connection () {
+  # NOTE: network interface may be in use by host => $(cat /sys/class/net/${INTERFACE}/operstate)" !== "up")
+  # NOTE: wifi has a delay up to 1 min
+  # wifi container running
+  [ "$(docker inspect -f '{{.State.Running}}' ${WIFI_CONTAINER} 2> /dev/null)" = "true" ] && \
+  # Check interface variable is set
+  [ ! -z $(docker exec -it $WIFI_CONTAINER iw dev | grep 'Interface' | awk 'NR==1{print $2}') ] && \
+  create_connection_message "Wi-Fi" "$WIFI_GET_CREDS" && \
+  exit 0 || echo "Wifi not available"
+}
+
+function avahi_connection () {
+  # Ping to avahi endpoint: -c: number of pings. -w: timeout
+  avahi-resolve -n $AVAHI_ENDPOINT > /dev/null 2>&1 || { echo "Avahi-daemon not available" ; return ; }
+  # Https container exists
+  [ "$(docker ps -a | grep ${HTTPS_CONTAINER})" ] && \
+  # Https container running
+  [ "$(docker inspect -f '{{.State.Running}}' ${HTTPS_CONTAINER})" = "true" ] && \
+  # Https env variable LOCAL_PROXYING="true"
+  [ "$(docker exec -i ${HTTPS_CONTAINER} sh -c 'echo "$LOCAL_PROXYING"')" = "true" ] && \
+  # avahi-daemon running => systemctl is-active avahi-daemon RETURNS "active" or "inactive"
+  [ "$(systemctl is-active avahi-daemon)" = "active" ] && \
+  echo -e "\n\e[32mConnect to DAppNode through avahi-daemon.\e[0m\n\nVisit \e[4m$DAPPNODE_ADMINUI_LOCAL_URL\e\n\n[0mCheck out all the access methods available to connect to your DAppNode at \e[4m$DAPPNODE_WELCOME_URL\e[0m\n" && \
+  exit 0 || echo "Avahi-daemon not available"
+}
+
+function wireguard_connection () {
+  # wireguard container exists
+  [ "$(docker ps -a | grep ${WIREGUARD_CONTAINER})" ] && \
+  # wireguard container running
+  [ "$(docker inspect -f '{{.State.Running}}' ${WIREGUARD_CONTAINER})" = "true" ] && \
+  create_connection_message "Wireguard" "$($WIREGUARD_GET_CREDS)" && \
+  exit 0 || echo "Wireguard not available"
+}
+
+function openvpn_connection () {
+  # openvpn container exists
+  [ "$(docker ps -a | grep ${OPENVPN_CONTAINER})" ] && \
+  # openvpn container running
+  [ "$(docker inspect -f '{{.State.Running}}' ${OPENVPN_CONTAINER})" = "true" ] && \
+  create_connection_message "Open-VPN" "$($OPENVPN_GET_CREDS)" && \
+  exit 0 || echo "Open-VPN not available"
+}
+
+########
+#2.MAIN#
+########
+
+dappnode_startup_delay
+wifi_connection
+avahi_connection
+wireguard_connection
+openvpn_connection
+
+echo -e "\e[33mWARNING: no connection services available\e[0m"
+exit 0
---
+++ build/upgrades/upgrade_007.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Upgrade from 0.2.44 to 0.2.45
+
+
+DAPPNODE_DIR="/usr/src/dappnode"
+HOST_SCRIPTS_DIR="$DAPPNODE_DIR/scripts"
+DAPPNODE_ACCESS_CREDENTIALS="$HOST_SCRIPTS_DIR/dappnode_access_credentials.sh"
+DAPPNODE_PROFILE="$DAPPNODE_DIR/DNCORE/.dappnode_profile"
+LOGS_DIR="$DAPPNODE_DIR/logs"
+
+# NEW ALIASES in .dappnode_profile
+# The patron: '"'"' allows to escape single quote in sed
+grep -qF 'dappnode_wifi' $DAPPNODE_PROFILE ||
+    sed -i '/alias dappnode_connect/a alias dappnode_wifi='"'"'cat /usr/src/dappnode/DNCORE/docker-compose-wifi.yml | grep "SSID\\\|WPA_PASSPHRASE"'"'"'' $DAPPNODE_PROFILE
+
+grep -qF 'dappnode_wireguard' $DAPPNODE_PROFILE ||
+    sed -i '/alias dappnode_connect/a alias dappnode_wireguard='"'"'docker exec -i DAppNodeCore-api.wireguard.dnp.dappnode.eth getWireguardCredentials'"'"'' $DAPPNODE_PROFILE
+
+# NEW SCRIPT: dappnode_access_credentials.sh
+[ ! -f $DAPPNODE_ACCESS_CREDENTIALS ] || [ ! -f $DAPPNODE_PROFILE ] && \
+echo ".dappnode_profile OR dappnode_access_credentials.sh do not exist" && exit 1
+
+grep -qF "dappnode_access_credentials" $DAPPNODE_PROFILE || echo "/bin/bash ${DAPPNODE_ACCESS_CREDENTIALS}" >>$DAPPNODE_PROFILE
+
+# LOGS MIGRATION
+ls -1 *.log &>/dev/null && \
+mkdir -p $LOGS_DIR && \
+mv "$DAPPNODE_DIR/*.log" $LOGS_DIR || \
+echo "Logs migration already done"
