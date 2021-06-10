a16c52b88a7d6cea6d1cccab359195dea876e1e6
pablo <mendez4a@gmail.com>
2021-06-10 08:38:41 +0200

    Added profile as metadata

--- build/upgrades/upgrade_007.sh
+++ build/upgrades/upgrade_007.sh
@@ -2,29 +2,26 @@
 
 # Upgrade from 0.2.44 to 0.2.45
 
-
+GITHUB_URL="https://github.com/dappnode/DAppNode/releases/download/v0.2.44/"
 DAPPNODE_DIR="/usr/src/dappnode"
+DAPPNODE_DNCORE_DIR="$DAPPNODE_DIR/DNCORE"
 HOST_SCRIPTS_DIR="$DAPPNODE_DIR/scripts"
 DAPPNODE_ACCESS_CREDENTIALS="$HOST_SCRIPTS_DIR/dappnode_access_credentials.sh"
-DAPPNODE_PROFILE="$DAPPNODE_DIR/DNCORE/.dappnode_profile"
+DAPPNODE_PROFILE="$DAPPNODE_DNCORE_DIR/.dappnode_profile"
 LOGS_DIR="$DAPPNODE_DIR/logs"
 
 # NEW ALIASES in .dappnode_profile
-# The patron: '"'"' allows to escape single quote in sed
-grep -qF 'dappnode_wifi' $DAPPNODE_PROFILE ||
-    sed -i '/alias dappnode_connect/a alias dappnode_wifi='"'"'cat /usr/src/dappnode/DNCORE/docker-compose-wifi.yml | grep "SSID\\\|WPA_PASSPHRASE"'"'"'' $DAPPNODE_PROFILE
-
-grep -qF 'dappnode_wireguard' $DAPPNODE_PROFILE ||
-    sed -i '/alias dappnode_connect/a alias dappnode_wireguard='"'"'docker exec -i DAppNodeCore-api.wireguard.dnp.dappnode.eth getWireguardCredentials'"'"'' $DAPPNODE_PROFILE
+# 1. Modify existng profile. New profiles contains alias dappnode_wifi, old ones not
+mkdir -p $DAPPNODE_DNCORE_DIR
+grep -qF "dappnode_wifi" $DAPPNODE_PROFILE || cp -rf /usr/src/app/hostScripts/.dappnode_profile $DAPPNODE_PROFILE
 
 # NEW SCRIPT: dappnode_access_credentials.sh
-[ ! -f $DAPPNODE_ACCESS_CREDENTIALS ] || [ ! -f $DAPPNODE_PROFILE ] && \
-echo ".dappnode_profile OR dappnode_access_credentials.sh do not exist" && exit 1
-
+mkdir -p $HOST_SCRIPTS_DIR
+[ -f $DAPPNODE_ACCESS_CREDENTIALS ] || cp -rf /usr/src/app/hostScripts/.dappnode_access_credentials.sh $DAPPNODE_ACCESS_CREDENTIALS
 # Call dappnode_access_credentials.sh script on every session
 grep -qF "dappnode_access_credentials" $DAPPNODE_PROFILE || echo "/bin/bash ${DAPPNODE_ACCESS_CREDENTIALS}" >>$DAPPNODE_PROFILE
 
-# LOGS MIGRATION
+# LOGS MIGRATION: logs dir was changed to /usr/src/dappnode/logs/
 ls -1 *.log &>/dev/null && \
 mkdir -p $LOGS_DIR && \
 mv "$DAPPNODE_DIR/*.log" $LOGS_DIR || \
