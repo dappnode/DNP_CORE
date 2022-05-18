07637e31f0a7e0a6ee2965642d8ca64f6a2b0b94
dapplion <35266934+dapplion@users.noreply.github.com>
2022-05-19 00:49:45 +0700

    Add src to docker-compose

--- src/build/upgrades/upgrade_002.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Upgrade from 0.2.15 to 0.2.16
-grep -qF 'exec DAppNodeCore-vpn\.dnp\.dappnode\.eth getAdminCredentials' /usr/src/dappnode/DNCORE/.dappnode_profile || \
-sed  -i "s,docker exec DAppNodeCore-vpn.dnp.dappnode.eth getAdminCredentials,docker run --rm -v dncore_vpndnpdappnodeeth_data:/usr/src/app/secrets \$\(docker inspect DAppNodeCore-vpn.dnp.dappnode.eth --format '{{.Config.Image}}'\) getAdminCredentials," /usr/src/dappnode/DNCORE/.dappnode_profile
--- src/build/upgrades/upgrade_003.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Run script on the host with the nsenter tool
-docker run --rm --privileged --pid=host -t alpine:3.8 nsenter -t 1 -m -u -n -i /usr/src/dappnode/DNCORE/scripts/upgrade/install_usbmount.sh
