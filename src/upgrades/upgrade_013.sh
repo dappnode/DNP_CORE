d769d22391bf1fa1c86104ad4c8e7757e17f7ab5
Diego <dsimog01@estudiantes.unileon.es>
2023-07-21 14:02:58 +0200

    Disable automatic reboots

--- src/upgrades/upgrade_013.sh
+++ src/upgrades/upgrade_013.sh
@@ -16,14 +16,14 @@ fi
 # Modifies a config file
 modify_config_file() {
     local config_file="$1"
-    local config_setting="$2"
-    if ! grep -q "^$config_setting" "$config_file"; then
-        # Add setting if not present
-        echo "$config_setting" >> "$config_file"
-        echo "Added setting to $config_file"
-    fi
+    local config_setting_key="$2"
+    local config_setting_value="$3"
+    # Remove any appearances of the key from the file
+    sed -i "/^$config_setting_key .*/d" "$config_file"
+    # Add the updated setting
+    echo "$config_setting_key \"$config_setting_value\";" >> "$config_file"
+    echo "Modified setting $config_setting_key in $config_file"
 }
-
 
 # Check and configure unattended-upgrades config file
 unattended_config_file="/etc/apt/apt.conf.d/50unattended-upgrades"
@@ -32,23 +32,27 @@ if [ ! -f "$unattended_config_file" ]; then
     exit 1
 fi
 
-# Enable automatic removal of unused dependencies
-modify_config_file "$unattended_config_file" 'Unattended-Upgrade::Remove-Unused-Dependencies "true";'
+echo "Unattended-upgrades config file ($unattended_config_file) exists"
+
+# Enable automatic removal of unused dependencies and disable automatic reboot
+modify_config_file "$unattended_config_file" 'Unattended-Upgrade::Remove-Unused-Dependencies' 'true'
+modify_config_file "$unattended_config_file" 'Unattended-Upgrade::Automatic-Reboot' 'false'
 
 # Check and configure auto-upgrades config file
 auto_upgrades_file="/etc/apt/apt.conf.d/20auto-upgrades"
 if [ ! -f "$auto_upgrades_file" ]; then
     # Create the file
+    echo "Auto upgrades file ($auto_upgrades_file) does not exist. Creating it..."
     echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
     dpkg-reconfigure -f noninteractive unattended-upgrades
 
     # Check if the file was created
     if [ ! -f "$auto_upgrades_file" ]; then
-        echo "Error: $auto_upgrades_file was not created"
+        echo "Error: $auto_upgrades_file could not be created"
         exit 1
     fi
 fi
 
 # Enable automatic updates and unattended-upgrades (file should exist now)
-modify_config_file "$auto_upgrades_file" 'APT::Periodic::Update-Package-Lists "1";'
-modify_config_file "$auto_upgrades_file" 'APT::Periodic::Unattended-Upgrade "1";'
+modify_config_file "$auto_upgrades_file" 'APT::Periodic::Update-Package-Lists' '1'
+modify_config_file "$auto_upgrades_file" 'APT::Periodic::Unattended-Upgrade' '1'
