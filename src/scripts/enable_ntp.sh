2ea93ecae575de874f3bca7f50d3c78f1cf2f37a
Diego <dsimog01@estudiantes.unileon.es>
2023-09-12 09:52:49 +0200

    Replace NTP by chrony

--- src/scripts/enable_ntp.sh
+++ src/scripts/enable_ntp.sh
@@ -3,24 +3,27 @@
 ## Motivation:
 ##    We have seen that clients might lose sync if the clock is not properly synchronized
 ## Reference:
-##    https://wiki.debian.org/NTP
+##    https://chrony-project.org/
 
-# If the clock is synchronized with NTP servers, the script will exit
-NTP_SYNCED=$(timedatectl show --property=NTPSynchronized --value)
-if [ "$NTP_SYNCED" = "yes" ]; then
+# If the clock is synchronized with NTP servers using chrony, the script will exit
+TIME_SYNCED=$(timedatectl show --property=NTPSynchronized --value)
+if [ "$TIME_SYNCED" = "yes" ]; then
     echo "Clock is already synchronized with NTP servers."
     exit 0
 fi
 
-# Install or update ntp package
+# Uninstall ntp
+apt-get purge -y ntp
+
+# Install or update chrony package
 apt-get update
-apt-get install -y ntp
+apt-get install -y chrony
 
-# Enable on boot and restart ntp service
-sudo systemctl enable ntp
-sudo systemctl restart ntp
+# Enable on boot and restart chronyd service
+sudo systemctl enable chrony
+sudo systemctl restart chrony
 
-echo "Clock should now be synchronized with NTP servers."
+echo "Clock should now be synchronized with NTP servers using chrony."
 
 # Set system clock to UTC. Maintaining the RTC in the local timezone is not fully supported and will create various problems
 # with time zone changes and daylight saving adjustments. Reference: https://manpages.debian.org/unstable/systemd/timedatectl.1.en.html
